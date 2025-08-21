abac8a6ec9ed3f0e889b153ef7666615239eb54f
UsopKing7 <nicolas@gmail.com@gmail.com>
2025-08-21 02:06:02 -0400

    rutas testeadas correctamente

--- Backend/src/presentation/routes/auth.routes.ts
+++ Backend/src/presentation/routes/auth.routes.ts
@@ -5,6 +5,7 @@ import { accessAdminMiddleware } from '../middlewares/acces-admin.middleware'
 
 export const authRouter = Router()
 
+// Routes tested witch jest and supertest
 authRouter.post('/login', login)
 authRouter.post('/register', register)
 authRouter.get('/users/:id_user', accessAdminMiddleware(['ADMIN']), getUserById)
--- Backend/src/presentation/routes/cartItem.routes.ts
+++ Backend/src/presentation/routes/cartItem.routes.ts
@@ -3,4 +3,5 @@ import { createCartItem } from '../controllers/cartItem.controller'
 
 export const cartItemRouter = Router()
 
+// Route test with jest and supertest
 cartItemRouter.post('/create/cartItem/:id_cart/:id_product', createCartItem)
